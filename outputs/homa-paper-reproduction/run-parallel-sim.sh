436552dc1fcf617dc8ae9e3d8148db5efb6bf06f
Jackson Conte <jjconte@ucsd.edu>
2023-12-13 12:43:52 -0800

    first crack at bandwith util, numbers are wrong

--- outputs/homa-paper-reproduction/run-parallel-sim.sh
+++ outputs/homa-paper-reproduction/run-parallel-sim.sh
@@ -3,7 +3,7 @@ for load in 0.8 0.5
 do
     for simIdx in $(seq 0 $(($1-1))) 
     do 
-        nice ./waf --run "scratch/homa-paper-reproduction --load=$load --simIdx=$simIdx --duration=$2 --disableRtx" &
+        nice ./waf --run "scratch/HomaL4Protocol-paper-reproduction --load=$load --simIdx=$simIdx --duration=$2 --disableRtx" &
         sleep 10
     done
     wait
